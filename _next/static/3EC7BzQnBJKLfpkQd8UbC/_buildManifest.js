8c5de13cb8771149920fa3549b4ac3cbea73e2c4
jrm-muller~ <jerome.mlr40@gmail.com~>
2022-12-12 12:52:52 +0100

    Initial commit

--- _next/static/3EC7BzQnBJKLfpkQd8UbC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/909-e6246b04dae3ab30.js","static/css/5e9931d8c5eaefc9.css","static/chunks/pages/index-207ed1254dcdc39b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
