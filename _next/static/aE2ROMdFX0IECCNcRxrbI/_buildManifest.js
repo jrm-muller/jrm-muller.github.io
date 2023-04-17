9ef26a23ed4f22540b21d7fbd401e6627dce1065
jrm-muller <jerome.mlr40@gmail.com>
2023-04-17 12:25:35 +0200

    update J20 St Amand Colombelles

---
+++ _next/static/aE2ROMdFX0IECCNcRxrbI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/60f5f6dd3f8c5116.css","static/chunks/63-82eeaacb153345ab.js","static/chunks/pages/index-00d6e085ffe025cd.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
